66cb3bd434a59b33c1b26a9d8f372ca46dc8b9d3
manishthatte <manishjagdishthatte@gmail.com>
2016-03-16 23:48:36 +0530

    Added files via upload

---
+++ details.h
@@ -0,0 +1,17 @@
+#ifndef PERSONAL_DETAILS_H_INCLUDED
+#define PERSONAL_DETAILS_H_INCLUDED
+#define NAME "Name: Manish Thatte"
+#define ADDRESS "Address: C-101, Acropolis Apts., Thatte Nagar, Gangapur Road, Nashik - 422005"
+#define EMAIL_ID "email id: manishthatte@hotmail.com"
+#define WHATSAPP "whatsapp: +91 9028667940"
+#include <stdio.h>
+
+
+void details()
+{
+    printf("Program written by: \n"NAME"\n"ADDRESS"\n"EMAIL_ID"\n"WHATSAPP);
+
+}
+
+
+#endif // PERSONAL_DETAILS_H_INCLUDED
